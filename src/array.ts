b2199d7e15b58d333a0d2b4fbcfdcc7733a0a531
Mirek Rusin <mirek@me.com>
2022-07-02 16:23:11 +0200

    Skip checking array entries for unknowns.

--- src/array.ts
+++ src/array.ts
@@ -1,4 +1,5 @@
 import { ok, fail, failed, refail, type Refute, type Result } from './prelude.js'
+import unknown_ from './unknown.js'
 
 /** Combinator over an array. */
 const array =
@@ -6,6 +7,9 @@ const array =
     (values: unknown): Result<T[]> => {
       if (!Array.isArray(values)) {
         return fail(values, 'expected array')
+      }
+      if (a === unknown_) {
+        return ok(values)
       }
       for (let i = 0; i < values.length; i++) {
         const r = a(values[i])
