56adc53f62ae864c101ef2d44ecd8f9767dbb04a
Mirek Rusin <mirek@me.com>
2022-07-02 08:54:16 +0200

    Update.

--- tsrd.cjs
+++ tsrd.cjs
@@ -1,27 +1,25 @@
 const Fs = require('fs')
 const Path = require('path')
 
-const ensure =
-  path => {
-    const basename = Path.basename(path, '.js')
-    const tsrd = `/// <reference types="./${basename}.d.ts" />\n`
-    const content = Fs.readFileSync(path, 'utf8')
-    if (!content.startsWith(tsrd)) {
-      Fs.writeFileSync(path, tsrd + content)
-    }
-  }
-
-const scan =
+/** Prepends tripple-slash reference directives for js files to support typy information in deno. */
+const prepend =
   dir => {
     const files = Fs.readdirSync(dir)
     for (const file of files) {
       const path = Path.join(dir, file)
       if (Fs.statSync(path).isDirectory()) {
         scan(path)
-      } else if (Path.extname(path) === '.js') {
-        ensure(path)
+        continue
+      }
+      if (Path.extname(path) === '.js') {
+        const basename = Path.basename(path, '.js')
+        const tsrd = `/// <reference types="./${basename}.d.ts" />\n`
+        const content = Fs.readFileSync(path, 'utf8')
+        if (!content.startsWith(tsrd)) {
+          Fs.writeFileSync(path, tsrd + content)
+        }
       }
     }
   }
 
-scan('esm')
+prepend('esm')
