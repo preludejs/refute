e044449a48d57b0250e7782c7a8262487ddaeefe
Mirek Rusin <mirek@me.com>
2022-11-20 13:39:15 +0100

    fix(tuple): updating error messages

--- src/tuple.ts
+++ src/tuple.ts
@@ -4,15 +4,15 @@ const tuple =
   <T extends [...Refute<unknown>[]]>(...as: T): Refute<{ [I in keyof T]: Refuted<T[I]> }> =>
     (value: unknown) => {
       if (!Array.isArray(value)) {
-        return fail(value, 'expected tuple')
+        return fail(value, 'expected array')
       }
       if (value.length > as.length) {
-        return fail(value, `expected tuple length of not more than ${as.length}`)
+        return fail(value, `expected array not longer than ${as.length}`)
       }
       for (let i = 0; i < value.length; i++) {
         const r = as[i](value[i])
         if (failed(r)) {
-          return refail(r, `tuple entry at index ${i}`)
+          return refail(r, `at index ${i}`)
         }
       }
       return ok(value as { [I in keyof T]: Refuted<T[I]> })
